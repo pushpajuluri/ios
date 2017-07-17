72f813a05680ec6d34940162340b31bc654e3b85
pushpajuluri <pushpa.juluri@gmail.com>
2017-07-17 10:31:07 +0530

    Add files via upload

--- ViewController.swift
+++ ViewController.swift
@@ -17,7 +17,7 @@ class ProductModel {
     var productName = ""
 }
 
-class ViewController:UITableViewController {
+class ViewController: UIViewController,UITableViewDataSource,UITableViewDelegate {
 
     @IBOutlet weak var myTableView: UITableView!
     
@@ -26,24 +26,43 @@ class ViewController:UITableViewController {
     //var dataArray : [[String:String ]] = [["firstName" : " john" , "lastname" : "cena"],["firstName" : "vikram" , "lastname" : "varma"]]
     func getNames() -> [CategoriesModel]
     {
-    let catobj = CategoriesModel()
-    catobj.catName = "class1"
+        let catobj = CategoriesModel()
+        catobj.catName = "class1"
         catobj.catId = "1"
         categoriesArray.append(catobj)
+        
+        // adding products
+            let proModel = ProductModel()
+            proModel.productName = "krish"
+            catobj.products.append(proModel)
+
+        
+        
+        
         let catobj2 = CategoriesModel()
         catobj2.catName = "class2"
         catobj2.catId = "2"
-        categoriesArray.append(catobj2)
         // adding products
-      for product in productsArray   {
-           let proModel = ProductModel()
-        proModel.productName = "krish"
-        proModel.productName = "krish2"
-        catobj.products.append(proModel)
+            let proModel1 = ProductModel()
+            proModel1.productName = "Prasad"
+            catobj2.products.append(proModel1)
         
+        categoriesArray.append(catobj2)
         
-      }
-        return categoriesArray
+        let catobj3 = CategoriesModel()
+        catobj3.catName = "class3"
+        catobj3.catId = "3"
+        catobj3.products = productsArray
+        categoriesArray.append(catobj3)
+        
+        let catobj4 = CategoriesModel()
+        catobj4.catName = "class4"
+        catobj4.catId = "4"
+        catobj4.products = productsArray
+
+        categoriesArray.append(catobj4)
+        
+            return categoriesArray
     }
     
     override func viewDidLoad() {
@@ -57,27 +76,45 @@ class ViewController:UITableViewController {
         // Dispose of any resources that can be recreated.
     }
     
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        if(selectedIndex != -1)
+        {
+            let catObj = categoriesArray[selectedIndex]
+            return catObj.products.count + categoriesArray.count
+            
+        }
         return categoriesArray.count
     }
     
-    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
         if (selectedIndex == indexPath.row){
          return 100
         }
         else {
         return 40 }
     }
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    
+     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
       
         let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath) as! CustomCellTableViewCell
-        let obj = self.categoriesArray [indexPath.row]
-        let  proObj = self.productsArray[indexPath.row]
-        cell.firstLable.text = obj.catId
-        cell.secondLabel.text = obj.catName
-        return cell
+        let catObj = self.categoriesArray [indexPath.row]
+        let prodObj = catObj.products
+
+        if selectedIndex != -1 {
+            if prodObj.count > 0 {
+                if (selectedIndex + productsArray.count) < indexPath.row && ( indexPath.row > selectedIndex) {
+                
+//                    cell.firstLable.text = prodObj.pr
+//                    cell.secondLabel.text = obj.catName
+
+                }
+            }
+        }
+        
+                return cell
     }
-    override func tableView(_ tableView: UITableView, didDeselectRowAt indexPath: IndexPath) {
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         if (selectedIndex == indexPath.row){
         selectedIndex = -1
         }
